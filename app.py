f53d8649050725e0c7549f4531b92a84a7dfe49a
mohas95 <mohas.debbagh95@gmail.com>
2025-09-25 15:55:20 -0400

    shared frame variable to display, with threaded camera operation, structure as a project

--- app.py
+++ app.py
@@ -1,53 +1,21 @@
 import cv2
-
 from flask import Flask, Response, render_template_string
+from camera import Camera
 
 app = Flask(__name__)
-
-pipeline = (
-    "v4l2src device=/dev/video0 ! "
-    "image/jpeg, width=1280, height=720, framerate=30/1 ! "
-    "jpegdec ! videoconvert ! appsink"
-)
-
-#camera = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
-
-#camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
-
-camera = cv2.VideoCapture(0)
-
-
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Campi Server</title>
-    </head>
-    <body>
-        <img src="{{ url_for('video_feed') }}" width ="640" height="480">
-    </body>
-</html>
-
-"""
-
+camera = Camera()
 
 
 def generate_frames():
-    while True:
-        ret, frame = camera.read()
-
-        if not ret:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-
+    while true:
+        frame = camera.get_frame()
+        if frame:
             yield(b'--frame\r\n'
                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/')
 def index():
-    return render_template_string(html)
+    return render_template("index.html")
 
 @app.route('/video_feed')
 def video_feed():
---
+++ camera.py
@@ -0,0 +1,51 @@
+import cv2
+import threading
+
+class camera:
+    def __init__(self, device=0, width= 1280, height = 720, fps = 30):
+
+        self.fps = fps
+        self.cap = cv2.VideoCapture(self.device)
+        self.configuire(width, height, fps)
+        self.frame = None
+        self.lock = threading.Lock()
+        self.running = True
+        t = threading.Thread(target=self.update, daemon=True)
+        t.start()
+
+    def update(self):
+        while self.running:
+            ret, frame = self.cap.read()
+            if not ret:
+                continue
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+
+            with self.lock:
+                self.frame = buffer.tobytes()
+
+
+    def get_frame(self):
+        with self.lock:
+            return self.frame
+
+    def configure(self, width= None, height = None, fps = None):
+
+        with self.lock:
+            if width:
+                self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+                self.width = width
+            if height:
+                self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+                self.height = height
+            if fps:
+                self.cap.set(cv2.CAP_PROP_FPS, fps)
+                self.fps = fps
+
+    def change_device(self, device, width= 1280, height = 720, fps = 30):
+        with self.lock:
+            self.cap.release()
+            self.cap = cv2.VideoCapture(device)
+            self.device = device
+
+        
